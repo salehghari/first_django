85ddab8869465e145c9664e779093768eb7c2ec2
salehghari <salehghari8470@gmail.com>
2021-07-29 14:12:41 +0430

    using class based views

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,23 @@
+from django.db import models
+from django.views.generic import ListView, DetailView
 from django.core.paginator import Page, PageNotAnInteger, Paginator
 from django.shortcuts import render, get_object_or_404
 # from django.http import HttpResponse, JsonResponse
 from .models import Article, Category
+class ArticleList(ListView):
+    queryset = Article.objects.published()
+    paginate_by = 3
 
-def home(request, page=1):
-    articles_list = Article.objects.published()
-    paginator = Paginator(articles_list, 2)
-    articles = paginator.get_page(page)
-    context = {
-        "articles" : articles,
-    }
-    return render(request,"blog/home.html", context)
+# def detail(request, slug):
+#     context = {
+#         "article" : get_object_or_404(Article.objects.published(), slug=slug),
+#     }
+#     return render(request,"blog/detail.html", context)
 
-def detail(request, slug):
-    context = {
-        "article" : get_object_or_404(Article.objects.published(), slug=slug),
-    }
-    return render(request,"blog/detail.html", context)
+class ArticleDetail(DetailView):
+    def get_object(self):
+        slug = self.kwargs.get('slug')
+        return get_object_or_404(Article.objects.published(), slug=slug)
 
 def category(request, slug, page=1):
     category = get_object_or_404(Category, slug=slug, status=True)
--- myproject/urls.py
+++ myproject/urls.py
@@ -30,15 +30,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from blog.views import home, detail, category
+from blog.views import ArticleList, ArticleDetail, category
 
 
 app_name = 'blog'
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
-    path('page/<int:page>', home, name='home'),
-    path('article/<slug:slug>', detail, name="detail"),
+    path('', ArticleList.as_view(), name='home'),
+    path('page/<int:page>', ArticleList.as_view(), name='home'),
+    path('article/<slug:slug>', ArticleDetail.as_view(), name="detail"),
     path('category/<slug:slug>', category, name="category"),
     path('category/<slug:slug>/page/<int:page>', category, name='category'),
 ]
